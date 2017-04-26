1801d2aefc826c88b35a439f936a2c5bb62295ce
Hampus <Hampus@DESKTOP-6CRJOHR>
2017-04-26 17:47:56 +0200

    Added the controllers

---
+++ KomplMVC/WebGallery/Controllers/AlbumController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheData.Repositories;
+using WebGallery.Mapping;
+using WebGallery.Models;
+
+namespace WebGallery.Controllers
+{
+    [Authorize]
+    public class AlbumController : Controller
+    {
+        private AlbumRepository AlbumRepository { get; set; }
+        
+        private CommentRepository CommentRepository { get; set; }
+
+        public AlbumController()
+        {
+            this.AlbumRepository = new AlbumRepository();
+            
+            this.CommentRepository = new CommentRepository();
+        }
+        // GET: Album
+        public ActionResult Index()
+        {
+            //var pr = new PhotoRepository();
+            //pr.Clear();
+            //AlbumRepository.Clear();
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            var email = User.Identity.Name;
+
+            var model = AlbumRepository.Get(email).ToModel();
+            return PartialView("List", model);
+        }
+
+
+
+        // GET: Album/Create
+
+        // POST: Album/Create
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult Create(AlbumViewModel collection)
+        {
+            if (ModelState.IsValid)
+            {
+                collection.UserEmail = User.Identity.Name;
+                collection.Id = Guid.NewGuid();
+
+                AlbumRepository.AddOrUpdate(collection.ToEntity());
+            }
+
+            return List();
+        }
+
+        public ActionResult Comment(Guid id)
+        {
+            var comments = CommentRepository.Get(id).ToModel();
+
+            return PartialView("Comment", comments);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateComment(string comment, Guid AlbumRefId)
+        {
+            var model = new CommentViewModel();
+            model.UserEmail = User.Identity.Name;
+            model.AlbumRefId = AlbumRefId;
+            model.Id = Guid.NewGuid();
+            model.Text = comment;
+            CommentRepository.AddOrUppdate(model.ToEntity());
+
+            return Comment(AlbumRefId);
+        }
+
+        public ActionResult DeleteComment(Guid id)
+        {
+            CommentRepository.Delete(id);
+
+            return null;
+        }
+        // GET: Album/Edit/5
+        public ActionResult Edit(Guid id)
+        {
+            var model = AlbumRepository.Get(id).ToModel();
+
+            return View(model);
+        }
+
+        // POST: Album/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Guid id, AlbumViewModel collection)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    AlbumRepository.AddOrUpdate(collection.ToEntity());
+
+                    return RedirectToAction("Index");
+                }
+
+                return View(collection);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Album/Delete/5
+        public ActionResult Delete(Guid id)
+        {
+            AlbumRepository.Delete(id);
+
+            return null;
+        }
+
+        // POST: Album/Delete/5
+
+
+    }
+}
---
+++ KomplMVC/WebGallery/Controllers/PhotoController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheData.Repositories;
+using WebGallery.Mapping;
+using WebGallery.Models;
+
+namespace WebGallery.Controllers
+{
+    
+        public class PhotoController : Controller
+        {
+            private PhotoRepository PhotoRepository { get; set; }
+            private CommentRepository CommentRepository { get; set; }
+
+            public PhotoController()
+            {
+                PhotoRepository = new PhotoRepository();
+                CommentRepository = new CommentRepository();
+            }
+            // GET: Photo
+            public ActionResult Index(Guid id)
+            {
+                ViewBag.AlbumRefId = id;
+
+                return View();
+            }
+
+            // GET: Photo/Details/5
+            public ActionResult List(Guid id) // Album Id
+            {
+                var photos = PhotoRepository.Get(id);
+                return PartialView("List", photos.ToModel());
+            }
+
+
+
+            public ActionResult Create(Guid id) // Album id
+            {
+                ViewBag.AlbumRefId = id;
+                return PartialView();
+            }
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public ActionResult Create(PhotoViewModel model, HttpPostedFileBase file, Guid id)
+            {
+                if (ModelState.IsValid)
+                {
+                    if (file != null || file.ContentLength != 0)
+                    {
+                        model.Id = Guid.NewGuid();
+                        model.Url = @"/img/" + file.FileName;
+                        model.AlbumRefId = id;
+                        PhotoRepository.Add(model.ToEntity());
+
+                        file.SaveAs(Path.Combine(Server.MapPath("~/img"), file.FileName));
+                    }
+                }
+
+                return List(id);
+            }
+            public ActionResult Comment(Guid id)
+            {
+                var comments = CommentRepository.Get(id).ToModel();
+
+                return PartialView("Comment", comments);
+            }
+            public ActionResult DeleteComment(Guid id)
+            {
+                CommentRepository.Delete(id);
+
+                return null;
+            }
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public ActionResult CreateComment(string comment, Guid PictureRefId)
+            {
+                var model = new CommentViewModel();
+                model.UserEmail = User.Identity.Name;
+                model.PhotoRefId = PictureRefId;
+                model.Id = Guid.NewGuid();
+                model.Text = comment;
+                CommentRepository.AddOrUppdate(model.ToEntity());
+
+                return Comment(PictureRefId);
+            }
+            public ActionResult Delete(Guid id)
+            {
+                PhotoRepository.Delete(id);
+
+                return null;
+            }
+
+        }
+    }

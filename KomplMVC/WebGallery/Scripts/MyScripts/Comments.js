ce14a4bcbe3493159032601ae2a3f3fb8d6cb001
Hampus <Hampus@DESKTOP-6CRJOHR>
2017-05-02 10:53:12 +0200

    Scripts and major changes

---
+++ KomplMVC/WebGallery/Scripts/MyScripts/Comments.js
@@ -0,0 +1,46 @@
+﻿
+
+$(document).ready(function (e) {
+    setInterval(loadComments, 10000);
+});
+
+
+function loadComments() {
+
+    var commetnContainerPhoto = $(".js-CommetnContainerPhoto");
+    var commetnContainerAlbum = $(".js-CommetnContainerAlbum");
+
+    commetnContainerPhoto.each(function () {
+        var container = $(this);
+        var photoId = container.attr("data-photoId");
+        var data = { id: photoId };
+        $.ajax({
+            dataType: "html",
+            type: "GET",
+            url: "/Photo/Comment",
+            data: data,
+            success: function (data) {
+
+                container.html(data);
+
+            }
+        });
+
+    });
+    commetnContainerAlbum.each(function () {
+        var container = $(this);
+        var albumId = container.attr("data-AlbumId");
+        var data = { id: albumId };
+        $.ajax({
+            dataType: "html",
+            type: "GET",
+            url: "/Album/Comment",
+            data: data,
+            success: function (data) {
+                container.html(data);
+
+            }
+        });
+
+    });
+}
---
+++ KomplMVC/WebGallery/Scripts/MyScripts/Photo.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    var pictureForms = $(".PictureForm");
+
+    pictureForms.submit(function (e) {
+        var form = $(this);
+        e.preventDefault();
+
+
+        setTimeout(function () {
+
+        }, 1000);
+
+
+        $.ajax({
+            method: "POST",
+            url: form.attr('action'),
+            data: new FormData(form[0]),
+            dataType: "html",
+            beforeSend: function () {
+
+            },
+            success: function (data) {
+
+                form[0].reset();
+                $("#PhotoContainer").html(data);
+            },
+            complete: function () {
+                setTimeout(hideSpinner, 1000);
+            },
+            processData: false,
+            contentType: false
+        });
+    });
+})
